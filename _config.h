d352340e9e130b28db117f3f39fbfcc3be7aea74
Guilherme de Almeida Lima <84299555+guilhermealmeidaa@users.noreply.github.com>
2021-10-30 15:10:45 -0300

    Update _config.h

---
+++ _config.h
@@ -0,0 +1,11 @@
+//Coloquem aqui os pinos e nomes que vocês usarem
+
+#define LED1 9
+#define LED2 10
+#define Button1 11
+#define Button2 12
+#define Button3 13
+#define SHUT_1 0
+#define SHUT_2 1
+#define SHUT_3 2
+#define SHUT_4 3

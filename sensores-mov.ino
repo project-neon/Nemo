60985cc7c25b7ba11f052771be1d5f18c0ef7467
Guilherme de Almeida Lima <84299555+guilhermealmeidaa@users.noreply.github.com>
2021-10-31 11:44:32 -0300

    fix: corrigindo erros feat. la fiora
    mudei o nome de algumas variaveis para ficar mais didatico e declarei as variaveis de distancia no setup

--- sensores-mov.ino
+++ sensores-mov.ino
@@ -2,7 +2,7 @@
 #include <VL53L0X.h>
 #include "_config.h"
 
-VL53L0X sensor, sensor2;
+VL53L0X sensor1, sensor2;
 
 /*#define SHUT_1 0
 #define SHUT_2 1
@@ -23,36 +23,27 @@ void setup()
     delay(10);
 
     digitalWrite(SHUT_1, HIGH)
-    sensor.setAddress(0x25);
+    sensor1.setAddress(0x25);
 
     digitalWrite(SHUT_2, HIGH)
     sensor2.setAddress(0x27);
     
     pinMode(SHUT_1, INPUT);
  
-    sensor.init();
+    sensor1.init();
     sensor2.init();
-    sensor.setTimeout(600);
+    sensor1.setTimeout(600);
     sensor2.setTimeout(600);
     
-    //Talvez seja melhor iniciar as variáveis distance0 e distance1 aqui para elas não
-    //ficarem sendo iniciadas toda a vez no loop
+    int distance1 = sensor1.readRangeSingleMillimeters();
+    int distance2 = sensor2.readRangeSingleMillimeters();
     
-    //Esse próximo seria só uma recomendação, talvez trocar os nomes por:
-    //distance0 --> distance1
-    //distance1 --> distance2
-    //Para ficar mais parecido ao sensor e sensor2
-    //Embora talvez seja uma boa mudar também o "sensor" para
-    //sensor --> sensor1
 }
 
 void loop()
 {
 
-    int distance0 = sensor.readRangeSingleMillimeters();
-    int distance1 = sensor2.readRangeSingleMillimeters();
- 
-    Serial.print(distance0);
+    Serial.print(distance1);
     Serial.print(" - ");
-    Serial.println(distance1);
+    Serial.println(distance2);
 }

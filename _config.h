60f3ace6c753a881f34f959286a542fb40f5dadd
Guilherme de Almeida Lima <84299555+guilhermealmeidaa@users.noreply.github.com>
2021-11-30 11:42:35 -0300

    Update _config.h

--- _config.h
+++ _config.h
@@ -5,9 +5,17 @@
 #define Button1 11
 #define Button2 12
 #define Button3 13
-#define SHUT_1 0
-#define SHUT_2 1
-#define SHUT_3 2
-#define SHUT_4 3
-#define sensor1 6  
-#define sensor2 7
+#define SHUT_1 A0
+#define SHUT_2 A1
+#define SHUT_3 A2
+#define SHUT_4 A3
+#define sensor1 A6  
+#define sensor2 A7
+//motors
+#define PWMA 2
+#define AIN1 3	
+#define AIN2 4
+#define PWMB 5
+#define BIN1 6
+#define BIN2 7 
+#define STBY 8
